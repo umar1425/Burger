1fcda2d283163d2468f8a1e5bd3f2cb2e8f91a16
Umar Arshad <umararshad1995@gmail.com>
2020-09-11 22:03:50 -0400

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
 //connection info given by heroku jawsdb
 //on local machine use .env file, on heroku use config vars
@@ -19,6 +20,17 @@ connection.connect(function (err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else (
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  }):
+)
 
 // Export connection for our ORM to use.
 module.exports = connection;
